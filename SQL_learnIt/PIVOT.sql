962d4fabfb7ef4e3f642803e951da099a05d1ccd
JavierETpna <trevisan.parana@gmail.com>
2025-04-27 16:13:00 -0300

    Corrección de espacios

--- SQL_learnIt/PIVOT.sql
+++ SQL_learnIt/PIVOT.sql
@@ -1,10 +1,10 @@
 /**
 PIVOT
-1º Selecciono las columnas que van a quedar
-2º Hago un nuevo Select y lo nombro (T_Origen) para pivotarlo
-3º Pivoteo esa Tabla (T_Origen) indicando la transformación de
-   la culumna de resultados y los valores para los cuales quiero las columnas
-   los valores deben ir entre corchetes para indicar que son columnas
+1ï¿½ Selecciono las columnas que van a quedar
+2ï¿½ Hago un nuevo Select y lo nombro (T_Origen) para pivotarlo
+3ï¿½ Pivoteo esa Tabla (T_Origen) indicando la transformaciï¿½n de
+	la culumna de resultados y los valores para los cuales quiero las columnas
+	los valores deben ir entre corchetes para indicar que son columnas
 **/
 
 SELECT * FROM (
